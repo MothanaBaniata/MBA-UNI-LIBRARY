3b8adf98d92f1df35082495f158b09dbb1fbb467
MothanaBaniata <1998mothana@gmail.com>
2024-10-30 10:16:18 +0300

    Set up borrowing functionality and implement redirection to single page or login

--- New folder/resources/views/home/book.blade.php
+++ New folder/resources/views/home/book.blade.php
@@ -7,6 +7,7 @@
                     <h2><em>Items</em> Currently In The Market.</h2>
                 </div>
             </div>
+
             <div class="col-lg-6">
                 <div class="filters">
                     <ul>
@@ -22,35 +23,43 @@
 
 
                     @foreach ($data as $data)
-                    @endforeach
-                    <div class="col-lg-6 currently-market-item all msc">
-                        <div class="item">
-                            <div class="left-image">
-                                <img src="book/{{ $data->book_img }}" alt=""
-                                    style="border-radius: 20px; min-width: 195px;">
-                            </div>
-                            <div class="right-content">
-                                <h4>{{ $data->title }}</h4>
-                                <span class="author">
-                                    <img src="author/{{ $data->author_img }}" alt=""
-                                        style="max-width: 50px; border-radius: 50%;">
-                                    <h6>{{ $data->author_name }}</h6>
-                                </span>
-                                <div class="line-dec"></div>
-                                <span class="bid">
-                                    Current Available<br><strong>{{ $data->quantity }}</strong><br>
-                                </span>
+                        <div class="col-lg-6 currently-market-item all msc">
+                            <div class="item">
+                                <div class="left-image">
+                                    <img src="book/{{ $data->book_img }}" alt=""
+                                        style="border-radius: 20px; min-width: 195px;">
+                                </div>
+                                <div class="right-content">
+                                    <h4>{{ $data->title }}</h4>
+                                    <span class="author">
+                                        <img src="author/{{ $data->author_img }}" alt=""
+                                            style="max-width: 50px; border-radius: 50%;">
+                                        <h6>{{ $data->author_name }}</h6>
+                                    </span>
+                                    <div class="line-dec"></div>
+                                    <span class="bid">
+                                        Current Available<br><strong>{{ $data->quantity }}</strong><br>
+                                    </span>
 
-                                <div class="text-button">
-                                    <a href="details.html">View Item Details</a>
+                                    <div class="text-button">
+                                        <a href="{{ url('book_details', $data->id) }}">View Item Details</a>
+                                    </div>
+
+                                    </br>
+
+                                    <div class="">
+                                        <a class="btn btn-primary" href="{{ url('borrow_books', $data->id) }}">Apply to
+                                            Borrow</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-
+                    @endforeach
 
                 </div>
             </div>
         </div>
+
+
     </div>
 </div>
