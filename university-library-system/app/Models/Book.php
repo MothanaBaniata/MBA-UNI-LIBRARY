9a20febb90f78f99bf336cf72212428ae0f56d78
MothanaBaniata <1998mothana@gmail.com>
2024-11-12 22:14:53 +0300

    Set up the migrations and models

---
+++ university-library-system/app/Models/Book.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'category_id',
+        'author',
+        'total_copies',
+        'borrowed_copies',
+        'image',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function borrowings()
+    {
+        return $this->hasMany(Borrowing::class);
+    }
+}
